d1c4afc5afbdbb82e83b9dadf9d7340814ccb962
LizGuile <lizguile19@gmail.com>
2023-04-04 22:22:37 +0800

    crash course

---
+++ Web Fundamentals/SQL/User Records.sql
@@ -0,0 +1,16 @@
+SELECT id
+FROM `hackerhero_practice`.`users`;
+
+-- What's the query for creating this new database table with the fields above?
+-- What's the query for inserting new records into this table?  Write queries for inserting three fake users into the table (one query for each insert).
+-- What's the query for updating one of the user records?  For example, if you wanted to update the user record for id = 1, with some fake data, what would the query be?
+-- What query would you run for updating all of the user records with the last_name of 'Choi'?
+-- What query would you run for updating all the user records where ID is 3, 5, 7, 12, or 19?
+-- What's the query for deleting a user record where id = 1?
+-- What's the query for deleting the entire users records in the users table?
+-- What's the query for dropping the entire users table?  What's the difference between dropping the table and deleting all records?
+-- What's the query for altering the email field to have it be 'email_address' instead?
+-- What's the query for altering the id so that it's a BIGINT instead?
+-- What's the query for adding a new field to the users table called is_active where it's a Boolean (meaning it's either a 0 or a 1).  
+-- 		Imagine you wanted the default value of this to be 0 when a new record is created and you won't allow this field to ever be NULL.  
+-- 		With this in mind, now come up with a query.
